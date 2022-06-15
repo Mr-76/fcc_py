3b8a6cbcfe707e86f7698afe961b29787f22dafb
Mr-76 <thegreath76@gmail.com>
2022-06-15 09:34:14 -0300

    days done

--- TimeCalculator/TimeCalculator.py
+++ TimeCalculator/TimeCalculator.py
@@ -3,18 +3,73 @@ def add_time(hora_inicial,hora_adicional,day = ""):
                 "Friday", "Saturday", "Sunday"]
     
     #usar formato 24 horas
-    hora_inicial = hora_inicial.split(" ")
+    list_time = hora_inicial.split(" ")
+    list_time = list_time[0].split(":") + [list_time[1]]
+    list_time.append(day)
+    list_add_time = hora_adicional.split(":")
 
-    hora_inicial = hora_inicial[0].split(":")
+    print(list_time)
 
+    hour = int(list_time[0])
+    minute = int(list_time[1])
+    hour_add = int(list_add_time[0])
+    minute_add = int(list_add_time[1])
 
-    hora_inicial.append(day)
-    print(hora_inicial)
+    if (list_time[2] == "AM"):
+        add_12 = 0
+    elif(list_time[2] == "PM"):
+        add_12 = 12
+    else:
+        pass
 
+    if (hour == 12 and list_time[2] == "AM"):
+        hour = 0
+    elif (hour == 12 and list_time[2] == "PM"):
+        hour = 12
+        add_12 = 0
+    else:
+        pass
+   
+    hour  = hour + add_12 # converting to 24 h
+
+    final_hour = hour + hour_add 
+    final_minute = minute + minute_add
+
+    final_hour = (final_hour) + (final_minute//int(60)) 
+    
+    final_minute = (final_minute//60  + final_minute % 60) 
+    string_time_day = '' 
+    if (final_hour < 24):
+        final_hour = final_hour
+        string_time_day = ""
+    else:
+        days = final_hour//24
+        final_hour = final_hour % 24
+        if (days == 1):
+            string_time_day = "(next day)"
+        elif (days > 1):
+            string_time_day = ("{} days later".format(days))
+    
+    if (final_hour > 12 and final_hour != 24):
+        final_hour = (final_hour - 12)
+        string_time = "PM"
+    elif (final_hour < 12):
+        final_hour = (final_hour - 0)
+        string_time = "AM"
+    elif (final_hour == 24):
+        final_hour = 12
+        string_time = "AM"
+    elif (final_hour == 12):
+        final_hour = 12
+        string_time = "PM"
+    else:
+        print("dont know that case")
+    
+    return ("{}:{} {} {} ".format(final_hour,final_minute,string_time,string_time_day))
 
 def main():
-    add_time("3:00 PM", "3:10")
-    add_time("4:00 PM", "3:11","monday")
+    print(add_time("11:43 PM","24:20","tueSday"))
+    print(add_time("4:00 PM", "3:11","monday"))
 
 if  __name__ == "__main__":
     main()
